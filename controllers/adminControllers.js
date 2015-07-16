ef40e820957fbc0adff67192d56b6078a7ff4108
Orbotron <ErikDKarlsson8@gmail.com>
2015-07-16 00:02:22 -0700

    Starting listing 8-17. Going to start documenting soon

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,5 +1,5 @@
-angular.module("sportStoreAdmin")
-    .constant("authUrl", "http://localhost:8005/users/login")
+angular.module("sportsStoreAdmin")
+    .constant("authUrl", "http://localhost:2403/users/login")
     .controller("authCtrl", function ($scope, $http, $location, authUrl) {
 
         $scope.authenticate = function (user, pass) {
@@ -13,5 +13,17 @@ angular.module("sportStoreAdmin")
             }).error(function (error) {
                 $scope.authenticationError = error;
             });
+        };
+    })
+    .controller("mainCtrl", function($scope) {
+        $scope.screens = ["Products", "Orders"];
+        $scope.current = $scope.screens[0];
+
+        $scope.setScreen = function(index) {
+            $scope.current = $scope.screens[index];
+        };
+
+        $scope.getScreen = function() {
+            return $scope.current == "Products" ? "/views/adminProducts.html" : "/views/adminOrders.html";
         };
     });
